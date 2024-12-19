8b40f04bd5d510cac26bdbc854aa14ef2ca7a3c0
randycodes163 <randyromaraog0101@gmail.com>
2024-12-19 18:04:06 +0800

    OOP-Activity

---
+++ Activities/practiceDOM2.js
@@ -0,0 +1,10 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todolist");
+const filterOption = document.querySelector(".filter-todo");
+
+todoButton.addEventListener("click, addTodo");
+
+function addTodo(){
+     
+}
